aea98dd4cd2a419db599d1b2fee9bba9841c4606
Irshadi Bagasputro <bagasputro.irshadi@gmail.com>
2020-10-06 23:29:31 +0700

    Update action.ts

--- myApp/src/redux/action.ts
+++ myApp/src/redux/action.ts
@@ -1,3 +1,5 @@
+import { REDUCER_ACTION_MAP } from "./reducer.ts";
+
 export const setUserState = (payload : any) => {
-    return { type: 'SET_USER_STATE', payload}
+    return { type: REDUCER_ACTION_MAP.SET_USER_STATE, payload}
 }
